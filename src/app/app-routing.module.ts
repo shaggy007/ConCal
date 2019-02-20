e6771bb0c2cb06774767b7d7366400b673556539
shaggypandit <sagarsharma.world@gmail.com>
2019-02-20 17:49:55 +0530

    Update new page called Construction Calculator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './construction-calculator/construction-calculator.module#ConstructionCalculatorPageModule' },
+   { path: 'construction-calculator', loadChildren: './construction-calculator/construction-calculator.module#ConstructionCalculatorPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/construction-calculator/construction-calculator.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-construction-calculator',
+  templateUrl: './construction-calculator.page.html',
+  styleUrls: ['./construction-calculator.page.scss'],
+})
+export class ConstructionCalculatorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
